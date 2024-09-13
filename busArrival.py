80de877116c818d16c210dc06c4bb6fc09965605
glitchdr0p <91477302+glitchdr0p@users.noreply.github.com>
2024-09-13 16:17:52 +0800

    Image, busArrival func

---
+++ busArrival.py
@@ -0,0 +1,51 @@
+import requests
+import json
+from pprint import pprint
+from datetime import datetime, timezone
+import pytz
+import time
+#from tabulate import tabulate
+
+def getEtaData(stopId):
+  timeSort = []
+  routeSort = {}
+  requestData = requests.get("https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/" + stopId).json()
+  for dataset in requestData['data']:
+
+    arrData = 0
+    routeString = dataset['route']
+
+    if(dataset['eta'] == None): #final bus has departed
+
+      timeSort.append((routeString, -1, -1, -1))
+      arrData = (-1, -1, -1)
+      routeSort[routeString] = [arrData]
+
+    else:
+      #handle ETA values
+      etaLocal = datetime.fromisoformat(dataset['eta'])
+      etaDisplayTime = "{:02d}:{:02d}".format(etaLocal.hour, etaLocal.minute) #display time, also local time
+
+      currentEpoch = datetime.now().timestamp()
+      etaEpoch = etaLocal.astimezone(pytz.utc).timestamp()
+
+      timeTill = (int(etaEpoch) - int(currentEpoch))//60 #floor division: round DOWN to nearest integer
+
+
+      
+      
+      #determine route status
+      routeStatus = 1
+      if(dataset['rmk_en'] == 'Final Bus'): #final bus case (set routeStatus to 0)
+        routeStatus = 0
+
+      #append tuple: (route, eta, eta_countdown, status)
+      if timeTill >= 0:
+        timeSort.append((routeString, etaDisplayTime, timeTill, routeStatus))
+        if routeString in routeSort:
+          routeSort[routeString].append((etaDisplayTime, timeTill, routeStatus))
+        else:
+          routeSort[routeString] = [(etaDisplayTime, timeTill, routeStatus)]
+
+  timeSort.sort(key = lambda x: x[2])
+  return(timeSort, routeSort)
